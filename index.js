71a5643d5984f1755f322d97d7cc40236c932907
jamesteogh <jamesteogh@gmail.com>
2022-06-26 15:12:02 +0800

    3rd commit 26/6/22

--- index.js
+++ index.js
@@ -5,50 +5,61 @@ const generateQueryUrl = queryTerm => {
     return `https://api.twelvedata.com/time_series?interval=1min&apikey=03123b25aa2f4028818b13c9ea66f3a2&symbol=${queryTerm}&outputsize=3`
 }
 
-const formSubmitted = (e) => {
+const formSubmitted = async (e) => {
     e.preventDefault()
     const tickerInput = document.querySelector("#ticker-input").value
-    fetchData(tickerInput) // invoke fetchData function to do API call
-
+    const tickerData = await fetchData(tickerInput) // invoke fetchData function to do API call
+    console.log(tickerData.values[0].close);
     const priceInput = document.querySelector("#price-input").value
     const dateInput = document.querySelector("#date-input").value
-    renderData(priceInput, dateInput)
-    // rowData(dateInput)
+    const selectInput = document.querySelector("#input-select").value
+    const quantityInput = document.querySelector("#quantity").value
+    // renderData(priceInput, dateInput)
+    const tickerPrice = tickerData.values[0].close
+    const tickerSym = tickerData.meta.symbol 
+    rowData(dateInput, tickerSym, selectInput, quantityInput, priceInput, tickerPrice)
 }
 
-const fetchData = async(tickerInput) => {
+const fetchData = async(ticker) => {
     try {
-        const response = await fetch(generateQueryUrl(tickerInput))
+        const response = await fetch(generateQueryUrl(ticker))
         const data = await response.json()
-        renderStock(data)
-            // console.log(data);
-            // console.log(data.values[0].close)
+        return data
+    // console.log(data.values[0].close)
     } catch (err) {
         console.log('err: ', err)
     }
 }
 
 // append stock ticker and current price using div
-const renderStock = (data) => {
-    
-    const divEl = document.createElement("div")
-    const stockTicker = data.meta.symbol 
-    const currentPrice = data.values[0].close // last closed price of stock
-    divEl.innerHTML = stockTicker + " " + "$" + parseFloat(currentPrice) 
-    stockContainerEl.appendChild(divEl)
-}
+// const renderStock = (data) => { 
+//     const divEl = document.createElement("div")
+//     const stockTicker = data.meta.symbol 
+//     const currentPrice = data.values[0].close // last closed price of stock
+//     divEl.innerHTML = stockTicker + " " + "$" + parseFloat(currentPrice) 
+//     stockContainerEl.appendChild(divEl)
+// }
+// get data and return stock current price
+
 // append price and date using div
 const renderData = (priceInput, dateInput) => {
     const divEl = document.createElement("div")
     divEl.innerHTML = "$" + priceInput + " " + dateInput
-    stockContainerEl.appendChild(divEl)
-    
+    stockContainerEl.appendChild(divEl)    
 }
 
-// const rowData = (dateInput) => {
-//     const createTr = document.createElement("tr")
-//     createTr.innerHTML = <th scope="row">dateInput</th> // CHECK
-//     stockTableEl.appendChild(createTr)
-// }
+const rowData = (dateInput, tickerInput, selectInput, quantityInput, priceInput, currentPrice) => {
+    const createTr = document.createElement("tr")
+    createTr.classList.add("trInner")
+    createTr.innerHTML = '<th scope="row" class = "rowth"></th>' 
+    stockTableEl.append(createTr)
+    document.querySelector(".rowth").innerHTML = dateInput
+
+    const createTd = document.createElement("td")
+    createTd.innerHTML = tickerInput.toUpperCase() // Append Stock Ticker into the table
+    document.querySelector(".trInner").appendChild(createTd)
+    createTd.innerHTML = currentPrice // Append action Buy, Sell etc   
+    document.querySelector(".trInner").appendChild(createTd)
+}
 
 formEl.addEventListener("submit", formSubmitted)
